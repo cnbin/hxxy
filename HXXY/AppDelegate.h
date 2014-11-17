548a8e97c8cc2b98a2aa668b71b7b952e911f81c
Apple <apple@AppledeiMac.local>
2014-11-17 08:33:15 +0800

    Initial Commit

---
+++ HXXY/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HXXY
+//
+//  Created by Apple on 11/17/14.
+//  Copyright (c) 2014 华讯网络投资有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HXXY/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXXY
+//
+//  Created by Apple on 11/17/14.
+//  Copyright (c) 2014 华讯网络投资有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
